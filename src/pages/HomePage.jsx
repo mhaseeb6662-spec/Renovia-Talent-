1db38f5f92cd439d26328bb672a6cf5aa542573a
Abdul Haseeb <mhaseeb6662@gmail.com>
2026-08-18 08:45:13 +0500

    Redesign section into a modern executive technology section with integrated header and feature cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,6 @@ import CapabilityStrip from '../components/CapabilityStrip';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
 import WhyChooseUs from '../components/WhyChooseUs';
-import FinalCTA from '../components/FinalCTA';
 
 export const HomePage = ({ onSelectService }) => {
   return (
@@ -14,7 +13,6 @@ export const HomePage = ({ onSelectService }) => {
       <AboutSection />
       <ServicesSection onSelectService={onSelectService} />
       <WhyChooseUs />
-      <FinalCTA />
     </main>
   );
 };
